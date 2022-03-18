3aedd2587d3e3042b63f2484ab201f6a692e0e08
Paul Obuba <paulobuba26@gmail.com>
2022-03-18 09:38:11 +0100

    hacker-news

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+
+import React, { useState, useEffect } from 'react'
 import './App.css';
 
-function App() {
+
+const App = () => {
+  const [news, setNews] = useState([]);
+  const [search, setSearch] = useState('react');
+  const [url, setUrl] = useState('http://hn.algolia.com/api/v1/search?query=react');
+  const [loading, setLoading] = useState(false)
+
+  const fetchNews = () => {
+    setLoading(true)
+    fetch(url)
+      .then(response => response.json())
+      .then(data => (setNews(data.hits), setLoading(false)))
+      .catch(error => console.log(error))
+  }
+
+  useEffect(() => {
+    fetchNews()
+  }, [url])
+
+  const handleChange = (event) => {
+    setSearch(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setUrl(`http://hn.algolia.com/api/v1/search?query=${search}`);
+    setSearch('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='app'>
+      <h2>Hacker News</h2>
+      {loading ? 'Loading...' :
+        <form onSubmit={handleSubmit}>
+          <input type='text' value={search} onChange={handleChange} placeholder='search news' />          
+          <button type='submit'>submit</button>
+        </form>
+      }
+       {news.length === 0 && <h1>No Result Found...</h1>}
+
+      {news.map(item => <div key={item}>{item.title}</div>)}
     </div>
-  );
+  )
+
 }
 
 export default App;
+
+
